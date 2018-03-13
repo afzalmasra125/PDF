5a8d8439d9ea472681b902c4211eb34fa2c00552
afzalmasra125 <afzalmasra@gmail.com>
2018-03-12 21:18:44 -0500

    prawn

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -1,17 +1,19 @@
 class ExportsController < ApplicationController 
-  def show
-    respond_to do |format|
-      // some other formats like: format.html { render :show }
+  require 'unirest'
 
-      format.pdf do
-        pdf = Prawn::Document.new
-        pdf.text "Hellow World!"
+  def show
+    response = Unirest.get("http://localhost:3000/student/1")
+
+    # @resume = Student.find(params[:id])
+
+   response.respond_to do|format|
+     format.pdf do
+        pdf = ExportPdf.new
         send_data pdf.render,
           filename: "export.pdf",
           type: 'application/pdf',
           disposition: 'inline'
       end
     end
-  end
+  end 
 end  
-
